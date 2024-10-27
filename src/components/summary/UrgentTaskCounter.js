e2336c20b935ccb92a4a41a343f1d67f350e1cae
Guy Luong <guy.luong.7@gmail.com>
2024-10-27 13:05:46 +0100

    [CSS] Font weight and size + positioning

--- src/components/summary/UrgentTaskCounter.js
+++ src/components/summary/UrgentTaskCounter.js
@@ -1,13 +1,13 @@
 const UrgentTaskCounter = () => {
     return (
-        <div className="w-10/12 bg-blue-600 text-white task-general flex">
-            <div className="flex flex-col">
+        <div className="w-full bg-blue-600 text-white task-general flex items-center">
+            <div className="flex flex-1 flex-col justify-center items-center">
                 <img src="./assets/icons/summary/urgent-tasks.svg" className="task-counter-icon" alt="Urgent icon" />
                 <p>Urgent tasks</p>
             </div>
-            <div className="flex flex-col">
-                <p>Date...</p>
-                <p>Incoming deadline</p>
+            <div className="flex flex-2 flex-col">
+                <p className="font-semibold">Date...</p>
+                <p className="font-extralight text-sm">Incoming deadline</p>
             </div>
         </div>
     );
