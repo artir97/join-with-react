85be3310c7ba2b06a69e47619d55ccb4d5032723
Guy Luong <guy.luong.7@gmail.com>
2024-10-27 18:59:38 +0100

    [UI] Added colored initials icon for contacts

--- src/components/contacts/ContactLetterList.js
+++ src/components/contacts/ContactLetterList.js
@@ -1,3 +1,5 @@
+import NameIcon from "./NameIcon";
+
 const ContactLetterList = ({ letter, list }) => {
     return (
         <div className="w-full">
@@ -7,9 +9,8 @@ const ContactLetterList = ({ letter, list }) => {
             <div>
                 {list.sort((a, b) => a.name > b.name)
                     .map((info, i) => <div key={i} className="flex items-center py-4 space-x-4">
-                        <div className="rounded-full border border-gray-500 p-1 text-sm text-white bg-red-500">
-                            {info.name.split(" ").map(n => n.charAt(0).toUpperCase()).join("")}
-                        </div>
+
+                        <NameIcon name={info.name} />
                         <div className="flex flex-col">
                             <p>{info.name}</p>
                             <p className="text-blue-400 text-sm">{info.mail}</p>
---
+++ src/components/contacts/NameIcon.js
@@ -0,0 +1,17 @@
+const hue = (v1, v2) => {
+    console.log(v1, v2, "=>", Math.floor((v1 * 25 + v2) * 255 / (25 * 25)));
+    return Math.floor((v1 * 25 + v2) * 255 / (25 * 25));
+}
+
+const NameIcon = ({ name }) => {
+    const displayedName = name.split(" ").map(n => n.charAt(0).toUpperCase()).join("");
+    const values = name.split(" ").map(n => n.charAt(0).toUpperCase().charCodeAt(0) - 64);
+
+    return (
+        <div className="rounded-full p-1.5 text-sm text-white" style={{ backgroundColor: `hsl(${hue(values[0], values[1])}, 50%, 50%)` }}>
+            {displayedName}
+        </div>
+    )
+}
+
+export default NameIcon;
