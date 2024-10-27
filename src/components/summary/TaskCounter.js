4e7d2bdc768d6d7aa3487fad0e7a46e8568d0e49
Guy Luong <guy.luong.7@gmail.com>
2024-10-27 12:34:24 +0100

    [CSS] Task counter hover

--- src/components/summary/TaskCounter.js
+++ src/components/summary/TaskCounter.js
@@ -2,7 +2,7 @@ import './TaskCounter.css';
 
 const TaskCounter = ({iconUrl, name, count}) => {
     return (
-        <div className="task-general task-counter px-6 py-3 w-2/5 flex flex-col items-center">
+        <div className="task-general task-counter px-6 py-3 w-2/5">
             <div className="flex items-center">
                 <img src={iconUrl} className="task-counter-icon" alt="Just a placeholder"/>
                 <div className="text-4xl font-bold">{count}</div>
