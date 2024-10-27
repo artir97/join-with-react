eb201c58ae7cb3a66a477b78f7a012924a98252b
artir97 <85441675+artir97@users.noreply.github.com>
2024-10-27 12:35:08 +0100

    [Add] open dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,24 @@
+import {useState} from "react";
+import NavbarDropdown from "./NavbarDropdown";
+
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleClickDropDown = () => {
+        setIsOpen(true);
+    }
+
     return (
-        <nav>
-            <img src={'./assets/icon/mobile-nav-join-logo.png'} alt="logo"/>
-            <div className="nav-bar-user">
-                <div className="nav-bar-user-inner">
-                    <span>G</span>
+        <>
+            <nav>
+                <img src={'./assets/icon/mobile-nav-join-logo.png'} alt="logo"/>
+                <div onClick={handleClickDropDown} className="nav-bar-user">
+                    <div className="nav-bar-user-inner">
+                        <span>G</span>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+            {isOpen && <NavbarDropdown />}
+        </>
     );
 }
 
---
+++ src/components/NavbarDropdown.js
@@ -0,0 +1,12 @@
+const NavbarDropdown = () => {
+    return (
+        <div className="nav-bar-menu-dropdown">
+            <div className="dropdown-item">Help</div>
+            <div className="dropdown-item">Legal Notice</div>
+            <div className="dropdown-item">Privacy Policy</div>
+            <div className="dropdown-item">Log out</div>
+        </div>
+    );
+}
+
+export default NavbarDropdown;
