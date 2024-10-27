21fa59ad588640614bf3551fb37c18ff851bdcca
Guy Luong <guy.luong.7@gmail.com>
2024-10-27 14:42:54 +0100

    [CSS] Border + Transition in .task-general

--- src/components/summary/BigTaskCounter.js
+++ src/components/summary/BigTaskCounter.js
@@ -1,6 +1,6 @@
 const BigTaskCounter = ({count}) => {
     return (
-        <div className="bg-blue-200 w-full py-3 px-6 task-general flex space-x-4 items-center hover:rounded-ss-none hover:bg-white duration-300">
+        <div className="bg-blue-200 w-full py-3 px-6 task-general flex space-x-4 items-center hover:rounded-ss-none hover:bg-white">
             <img src="./assets/icons/summary/to-do.svg" alt="To-do icon" className="task-counter-icon ml-4"/>
             <p className="font-bold text-4xl text-black">{count}</p>
             <p className="font-light text-blue-500">Tasks To-do</p>
