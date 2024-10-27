600000e1c5448c2c1f5addf0e8e317f21bcc4db3
Guy Luong <guy.luong.7@gmail.com>
2024-10-27 14:45:32 +0100

    [CSS] Screen centered content

--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -4,18 +4,20 @@ import UrgentTaskCounter from "../components/summary/UrgentTaskCounter";
 
 const Summary = () => {
     return (
-        <div className="flex flex-col items-center space-y-4 p-4">
-            <UrgentTaskCounter count={1} date={"November 27, 2024"} />
-            <BigTaskCounter count={1} />
+        <div className="page-content grid place-items-center">
+            <div className="flex flex-col items-center space-y-4 p-4">
+                <UrgentTaskCounter count={1} date={"November 27, 2024"} />
+                <BigTaskCounter count={1} />
 
-            <div className="flex flex-col w-full space-y-4 justify-center">
-                <div className="space-x-4 flex w-full">
-                    <TaskCounter iconUrl="/assets/icons/summary/on-board.svg" name={"Tasks on board"} count={5} />
-                    <TaskCounter iconUrl="/assets/icons/summary/in-progress.svg" name={"Tasks in progress"} count={2} />
-                </div>
-                <div className="space-x-4 flex w-full">
-                    <TaskCounter iconUrl="/assets/icons/summary/awaiting-feedback.svg" name={"Awaiting feedback"} count={2} />
-                    <TaskCounter iconUrl="/assets/icons/summary/tasks-done.svg" name={"Tasks done"} count={5} />
+                <div className="flex flex-col w-full space-y-4 justify-center">
+                    <div className="space-x-4 flex w-full">
+                        <TaskCounter iconUrl="/assets/icons/summary/on-board.svg" name={"Tasks on board"} count={5} />
+                        <TaskCounter iconUrl="/assets/icons/summary/in-progress.svg" name={"Tasks in progress"} count={2} />
+                    </div>
+                    <div className="space-x-4 flex w-full">
+                        <TaskCounter iconUrl="/assets/icons/summary/awaiting-feedback.svg" name={"Awaiting feedback"} count={2} />
+                        <TaskCounter iconUrl="/assets/icons/summary/tasks-done.svg" name={"Tasks done"} count={5} />
+                    </div>
                 </div>
             </div>
         </div>
