43e1525bb93eb7465c5eacfdd27a66ca65297c80
Guy Luong <guy.luong.7@gmail.com>
2024-10-27 14:43:13 +0100

    [CSS] Spacing between icon and count

--- src/components/summary/TaskCounter.js
+++ src/components/summary/TaskCounter.js
@@ -3,7 +3,7 @@ import './TaskCounter.css';
 const TaskCounter = ({iconUrl, name, count}) => {
     return (
         <div className="task-general flex-1 task-counter py-3">
-            <div className="flex items-center">
+            <div className="flex items-center space-x-2">
                 <img src={iconUrl} className="task-counter-icon" alt="Just a placeholder"/>
                 <div className="text-4xl font-bold">{count}</div>
             </div>
