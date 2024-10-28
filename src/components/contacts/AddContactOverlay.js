6c0e74e699ebe98baa42dea2fc21287f8e54c346
Guy Luong <guy.luong.7@gmail.com>
2024-10-28 13:46:59 +0100

    [Refactor] BaseContactOverlay for editing and creating

--- src/components/contacts/AddContactOverlay.js
+++ src/components/contacts/AddContactOverlay.js
@@ -1,29 +1,13 @@
-import IconInput from '../base/IconInput';
+import BaseContactOverlay from './BaseContactOverlay';
 import './Overlay.css';
 
 const AddContactOverlay = ({ onExit }) => {
     return (
-        <div className="bg-opacity-50 bg-white mask">
-            <div className="p-4 absolute top-20 text-white right-10" onClick={onExit}>
-                X
-            </div>
-            <div className="rounded-2xl flex flex-col items-center mx-8 my-20 shadow-lg overlay bg-white box-border">
-                <div className="h-2/5 flex flex-col w-full space-y-2 items-center text-white justify-center bg-blue-600 rounded-t-2xl">
-                    <p className='font-bold text-4xl'>Add contact</p>
-                    <p className='font-light'>Tasks are better with a team!</p>
-                </div>
-                <form className='flex flex-col items-center space-y-4 px-6 py-12'>
-                    <IconInput className="input-contact" iconUrl="" placeholder="Name" />
-                    <IconInput className="input-contact" iconUrl="" placeholder="Mail address" />
-                    <IconInput className="input-contact" iconUrl="" placeholder="Phone number" />
-                    <button className='rounded bg-blue-500 text-white p-2 mt-4'>Create contact V</button>
-                </form>
-                <div className='center-icon bg-gray-300'>
-                    <img alt="Person icon"
-                        src="./assets/icons/contacts/person.svg" />
-                </div>
-            </div>
-        </div>
+        <BaseContactOverlay
+            title={"Add contact"}
+            flavorText={"Tasks are better with a team!"}
+            onExit={onExit}
+            isEditing={false} />
     );
 }
 
---
+++ src/components/contacts/BaseContactOverlay.js
@@ -0,0 +1,36 @@
+import IconInput from '../base/IconInput';
+import './Overlay.css';
+
+const BaseContactOverlay = ({ onSubmit, onExit, title, flavorText, isEditing }) => {
+    return (
+        <div className="bg-opacity-50 bg-white mask">
+            <div className="p-4 absolute top-20 z-30 text-white right-10" onClick={onExit}>
+                X
+            </div>
+            <div className="rounded-2xl flex flex-col items-center mx-8 my-20 shadow-lg overlay bg-white box-border">
+                <div className="h-2/5 flex flex-col w-full space-y-2 items-center text-white justify-center bg-blue-600 rounded-t-2xl">
+                    <p className='font-bold text-4xl'>{title}</p>
+                    <p className='font-light'>{flavorText}</p>
+                </div>
+                <form className='flex flex-col items-center space-y-4 px-6 py-12'>
+                    <IconInput className="input-contact" iconUrl="" placeholder="Name" />
+                    <IconInput className="input-contact" iconUrl="" placeholder="Mail address" />
+                    <IconInput className="input-contact" iconUrl="" placeholder="Phone number" />
+                    {isEditing
+                        ? <>
+                            <button className='rounded bg-white p-2 mt-4'>Delete</button>
+                            <button className='rounded bg-blue-500 text-white p-2 mt-4'>Save V</button>
+                        </>
+                        : <button className='rounded bg-blue-500 text-white p-2 mt-4'>Create contact V</button>
+                    }
+                </form>
+                <div className='center-icon bg-gray-300'>
+                    <img alt="Person icon"
+                        src="./assets/icons/contacts/person.svg" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default BaseContactOverlay;
