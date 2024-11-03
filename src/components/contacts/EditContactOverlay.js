f22c1941466520ebd8a4cb7b352cb7eb2ea135a1
Guy Luong <guy.luong.7@gmail.com>
2024-11-03 11:19:41 +0100

    [Feature] Edit contact works locally

--- src/components/contacts/EditContactOverlay.js
+++ src/components/contacts/EditContactOverlay.js
@@ -1,15 +1,24 @@
 import BaseContactOverlay from "./BaseContactOverlay";
 
-const EditContactOverlay = ({ onExit, name, mail, phone }) => {
+/**
+ * 
+ * @param {Object} props 
+ * @param {Function} props.onEditSubmit - Function to handle form submission. It takes one argument: {name, mail, phone}.
+ * @param {Function} props.onExit - Function to handle exit action. Triggered on the X icon.
+ * @param {Object} props.info - {name, mail, phone} - Contact info to prefill the form. 
+ * @returns 
+ */
+const EditContactOverlay = ({ onEditSubmit, onExit, info }) => {
     return (
         <BaseContactOverlay
             title={"Edit contact"}
             flavorText={"Tasks are better with a team!"}
             onExit={onExit}
             isEditing={true}
-            name={name}
-            mail={mail}
-            phone={phone} />
+            name={info.name}
+            mail={info.mail}
+            phone={info.phone}
+            onSubmit={onEditSubmit} />
     );
 }
 
