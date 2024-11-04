6439325442e5c47e30fd58c96e2ed4acbc2e425c
artir97 <85441675+artir97@users.noreply.github.com>
2024-11-04 19:02:48 +0100

    [Add] Routers and Links - switch in between pages

--- src/App.js
+++ src/App.js
@@ -7,23 +7,39 @@ import Contacts from './pages/Contacts';
 import AddTask from "./components/AddTask";
 import ContactInfo from './pages/ContactInfo';
 import Tasks from './pages/Tasks';
-import { DragProvider } from './contexts/DragContext';
+import {DragProvider} from './contexts/DragContext';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 // MAIN PAGE
 function App() {
-  return (
-    <DragProvider>
-      <Navbar />
-      <Tasks />
-      {/* <AddTask /> */}
-      {/* <Contacts /> */}
-      {/* <ContactInfo
-        name={"Anton Meyer"}
-        mail={"anton.meyer@somemail.com"}
-        phone={"This should not be a phone"} /> */}
-      <Footer />
-    </DragProvider>
-  );
+    return (
+        <Router>
+            <Navbar/>
+                <Switch>
+                    <Route path="/summary">
+                        <Summary/>
+                    </Route>
+                    <Route path="/tasks">
+                        <DragProvider>
+                            <Tasks/>
+                        </DragProvider>
+                    </Route>
+                    <Route path="/addTask">
+                        <AddTask/>
+                    </Route>
+                    <Route path="/contacts">
+                        <Contacts/>
+                    </Route>
+                    <Route path="/contactInfo">
+                        <ContactInfo
+                            name={"Anton Meyer"}
+                            mail={"anton.meyer@somemail.com"}
+                            phone={"This should not be a phone"}/>
+                    </Route>
+                </Switch>
+            <Footer/>
+        </Router>
+    );
 }
 
 export default App;
