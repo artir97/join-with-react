87049f5c0ac3751d63b2f754ebcdc33c067e1aaf
Guy Luong <guy.luong.7@gmail.com>
2024-11-05 11:55:43 +0100

    [Dev] Used map for Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,39 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+
+const links = [
+    {
+        path: "/summary",
+        iconUrl: "./assets/icon/footer/summary-icon.png",
+        name: "Summary"
+    },
+    {
+        path: "/tasks",
+        iconUrl: "./assets/icon/footer/board-icon.png",
+        name: "Board"
+    },
+    {
+        path: "/addtask",
+        iconUrl: "./assets/icon/footer/add-task-icon.png",
+        name: "Add Task"
+    },
+    {
+        path: "/contacts",
+        iconUrl: "./assets/icon/footer/contacts-icon.png",
+        name: "Contacts"
+    }
+];
 
 const Footer = () => {
     return (
         <footer>
-            <Link to="/summary">
-                <div>
-                    <img src={'./assets/icon/footer/summary-icon.png'} alt="summary-icon"/>
-                    <span>Summary</span>
-                </div>
-            </Link>
-
-            <Link to="/tasks">
-                <div>
-                    <img src={'./assets/icon/footer/board-icon.png'} alt="summary-icon"/>
-                    <span>Board</span>
-                </div>
-            </Link>
-
-            <Link to="/addtask">
-                <div>
-                    <img src={'./assets/icon/footer/add-task-icon.png'} alt="summary-icon"/>
-                    <span>Add Task</span>
-                </div>
-            </Link>
-
-            <Link to="/contacts">
-                <div>
-                    <img src={'./assets/icon/footer/contacts-icon.png'} alt="summary-icon"/>
-                    <span>Contacts</span>
-                </div>
-            </Link>
+            {links.map(l => (
+                <Link to={l.path}>
+                    <div>
+                        <img src={l.iconUrl} alt={`${l.name} icon`} />
+                        <span>{l.name}</span>
+                    </div>
+                </Link>
+            ))}
         </footer>
     );
 }
