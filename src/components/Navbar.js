47c02abd0039e366aea6080eafa8dd89cb2ef990
Guy Luong <guy.luong.7@gmail.com>
2024-11-05 13:07:39 +0100

    [UI] Desktop Navbar with SVG

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
-import {useState} from "react";
+import { useState } from "react";
 import NavbarDropdown from "./NavbarDropdown";
+import MobileSwitch from "./base/MobileSwitch";
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -9,8 +10,15 @@ const Navbar = () => {
 
     return (
         <>
-            <nav>
-                <img src={'./assets/icon/mobile-nav-join-logo.png'} alt="logo"/>
+            <nav className="flex space-x-2">
+                <img src={'./assets/icons/navbar/join.svg'} alt="logo" />
+                <div className="flex-1" />
+                <MobileSwitch desktopComponent={
+                    <>
+                        <span className="text-sm text-white font-light">Kanban Project Management Tool</span>
+                        <img src="./assets/icons/navbar/help.svg" alt="Help logo" />
+                    </>
+                } />
                 <div onClick={handleClickDropDown} className="nav-bar-user">
                     <div className="nav-bar-user-inner">
                         <span>G</span>
