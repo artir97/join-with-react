27e384087c7ce4698027f3136eafd5948dffa489
artir97 <85441675+artir97@users.noreply.github.com>
2024-11-05 16:58:30 +0100

    [Update] contact list get's rerendered on change

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,11 +1,9 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import { useContactList } from "../hooks/useContactList";
 
 import ContactLetterList from "../components/contacts/ContactLetterList";
 import AddContactOverlay from "../components/contacts/AddContactOverlay";
 import MainActionIcon from "../components/icons/MainActionIcon";
-
-const codeA = 'A'.charCodeAt(0);
 
 const list2 = [
     { name: "Tatiana Wolf", mail: "wolf@gmail.com", phone: "+49 2222 22 222 2" },
@@ -17,34 +15,31 @@ const list2 = [
 const Contacts = () => {
     const {list, changeContactList} = useContactList();
     const [showOverlay, setShowOverlay] = useState(false);
+    const [map2, setMap2] = useState({});
 
-    const [contacts, setContacts] = useState([...list]);
-    const [map, setMap] = useState(() => {
-        let map = contacts.reduce((acc, info) => {
+    useEffect(() => {
+        const codeA = 'A'.charCodeAt(0);
+        const newMap = list.reduce((acc, info) => {
             const initial = info.name.charAt(0).toUpperCase();
-
             if (!acc[initial]) {
                 acc[initial] = [];
             }
-
             acc[initial].push(info);
             return acc;
         }, {});
 
         // Fill all empty letters in the map with [] (empty array).
         Array.from({ length: 26 }, (_, i) => String.fromCharCode(codeA + i))
-            .filter(letter => !map[letter])
-            .forEach(letter => map[letter] = []);
+            .filter(letter => !newMap[letter])
+            .forEach(letter => newMap[letter] = []);
 
-        console.log(map);
+        setMap2(newMap);
+    }, [list]);
 
-        return map;
-    });
 
     const handleAddSubmit = (info) => {
         const initial = info.name.charAt(0).toUpperCase();
-        setMap(map => {return {...map, [initial]: [...map[initial], info]}});
-        setContacts(contacts => [...contacts, info]);
+        setMap2(map => {return {...map, [initial]: [...map[initial], info]}});
     }
 
     return (
@@ -52,7 +47,7 @@ const Contacts = () => {
             <button onClick={() => changeContactList(list2)} className="border-4 bg-black text-white">TEST</button>
             <button onClick={() => console.log(list)}>CLICK</button>
             <div className="page-content overflow-y-scroll">
-                {[...Object.entries(map)]
+                {[...Object.entries(map2)]
                     .filter(e => e[1].length > 0)
                     .sort((a, b) => a[0].charCodeAt(0) - b[0].charCodeAt(0))
                     .map((e, i) => <ContactLetterList key={i} letter={e[0]} list={e[1]} />)}
