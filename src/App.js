a58fa916b0e55f34d6e2c9d9bd48b36029878bf4
Guy Luong <guy.luong.7@gmail.com>
2024-11-07 16:35:47 +0100

    [UI] Add log in page

--- src/App.js
+++ src/App.js
@@ -1,47 +1,45 @@
-import './App.css';
-import './output.css';
-import Navbar from './components/Navbar';
-import Footer from "./components/Footer";
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { DragProvider } from './contexts/DragContext';
+import { ContactsProvider } from "./contexts/contactsContext";
+
+import BaseWrapper from './components/BaseWrapper';
 import Summary from './pages/Summary';
 import Contacts from './pages/Contacts';
 import AddTask from "./pages/AddTask";
 import ContactInfo from './pages/ContactInfo';
 import Tasks from './pages/Tasks';
-import { DragProvider } from './contexts/DragContext';
-import { ContactsProvider } from "./contexts/contactsContext";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import MobileSwitch from './components/base/MobileSwitch';
-import Sidebar from './components/Sidebar';
 import Help from './pages/Help';
+import LogIn from './pages/LogIn';
+
+import './App.css';
+import './output.css';
 
 // MAIN PAGE
 function App() {
   return (
-    <BrowserRouter>
-      <ContactsProvider> {/* Move ContactsProvider here */}
-        <div className='h-screen w-screen'>
-          <Navbar />
-          <div className='lg:flex lg:full-page-fit'>
-            <MobileSwitch desktopComponent={<Sidebar />} />
-            <div className='lg:flex-1'>
-              <Routes>
-                <Route path="/summary" element={<Summary />} />
-                <Route path="/tasks" element={
-                  <DragProvider>
-                    <Tasks />
-                  </DragProvider>
-                } />
-                <Route path="/addTask" element={<AddTask />} />
-                <Route path="/contacts" element={<Contacts />} />
-                <Route path="/contact-info/:email" element={<ContactInfo />} />
-                <Route path="/help" element={<Help />} />
-              </Routes>
-            </div>
-          </div>
-          <MobileSwitch mobileComponent={<Footer />} />
-        </div>
-      </ContactsProvider>
-    </BrowserRouter>
+    <div className='h-screen w-screen'>
+      <BrowserRouter>
+        <ContactsProvider> {/* Move ContactsProvider here */}
+          <Routes>
+            <Route path='/' element={<BaseWrapper />}>
+              <Route path="/summary" element={<Summary />} />
+              <Route path="/tasks" element={
+                <DragProvider>
+                  <Tasks />
+                </DragProvider>
+              } />
+              <Route path="/addTask" element={<AddTask />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/contact-info/:email" element={<ContactInfo />} />
+              <Route path="/help" element={<Help />} />
+            </Route>
+            <Route path='/login' element={<LogIn />} />
+          </Routes>
+        </ContactsProvider>
+      </BrowserRouter>
+    </div>
   );
 }
 
