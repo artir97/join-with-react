7e5d21a61414820fbe7f9ab3e0b0322908ed3f3d
Guy Luong <guy.luong.7@gmail.com>
2024-11-09 14:53:28 +0100

    [CSS] Spacing between page links and privacy

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,9 +3,11 @@ import LegalAndPrivacy from "./LegalAndPrivacy";
 
 const Sidebar = () => {
     return (
-        <div className="flex-col p-4 space-x-4 shadow-md w-56 text-gray-600">
-            <PageLinks />
-            <LegalAndPrivacy vertical/>
+        <div className="flex flex-col p-4 justify-between shadow-md w-56 text-gray-600">
+            <div>
+                <PageLinks />
+            </div>
+            <LegalAndPrivacy vertical />
         </div>
     );
 }
