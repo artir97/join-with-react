d5caa44bc29ed29f0abfaa01d57044355b45f6c1
Guy Luong <guy.luong.7@gmail.com>
2024-11-09 11:04:01 +0100

    [Rename] contactsContext -> ContactsContext

--- src/hooks/useContactList.js
+++ src/hooks/useContactList.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import {ContactsContext} from "../contexts/contactsContext";
+import { ContactsContext } from "../contexts/ContactsContext";
 
 export const useContactList = () => {
     const context = useContext(ContactsContext);
