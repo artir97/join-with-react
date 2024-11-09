31a078d636cb75f82aeeb5bfe29aef9a887881d2
artir97 <85441675+artir97@users.noreply.github.com>
2024-11-09 16:35:57 +0100

    [update] show prefilled task data in edit task overlay

--- src/components/tasks/EditTaskOverlay.js
+++ src/components/tasks/EditTaskOverlay.js
@@ -1,11 +1,20 @@
+import BaseTaskOverlay from "./BaseTaskOverlay";
 
 const EditTaskOverlay = ({ task }) => {
-
+    console.log(task);
 
     return (
         <div className="mask bg-white bg-opacity-50">
             <div className="rounded-2xl flex flex-col items-center mx-8 my-20 shadow-lg overlay fixed-h-edit-task-overlay bg-white box-border">
-
+                <BaseTaskOverlay
+                    name={task.name}
+                    description={task.description}
+                    date={task.dueDate}
+                    selectedPriority={task.priority}
+                    assigneesArr={task.assignees}
+                    subtasksArr={task.subtasks}
+                    taskCategory={task.category}
+                />
             </div>
         </div>
     );
