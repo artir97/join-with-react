e6b7b3362486ff757e74565387bf1766b52760e9
artir97 <85441675+artir97@users.noreply.github.com>
2024-11-12 16:06:36 +0100

    show actual tasks count in summary

--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -2,6 +2,8 @@ import MobileSwitch from "../components/base/MobileSwitch";
 import BigTaskCounter from "../components/summary/BigTaskCounter";
 import TaskCounter from "../components/summary/TaskCounter";
 import UrgentTaskCounter from "../components/summary/UrgentTaskCounter";
+import {useTasks} from "../hooks/useDataContext";
+
 
 const MobileSummary = () => (
     <div className="flex flex-col items-center space-y-4 p-4">
@@ -34,29 +36,50 @@ const getTimeOfDay = () => {
     }
 }
 
-const DesktopSummary = ({ name = "" }) => (
+const getTasks = (status, taskList) => {
+    switch (status) {
+        case "To do":
+            return taskList.filter(task => task.status === "To do");
+        case "In progress":
+            return taskList.filter(task => task.status === "In progress");
+        case "Awaiting feedback":
+            return taskList.filter(task => task.status === "Awaiting feedback");
+        case "Done":
+            return taskList.filter(task => task.status === "Done");
+        default:
+            return taskList;
+    }
+};
+
+const getUrgentTasks = (taskList, priority = "urgent") => {
+    return taskList.filter(task => task.priority === priority);
+};
+
+const DesktopSummary = ({ name = "", taskList }) => (
     <div className="flex flex-col items-center space-y-10 p-4">
         {name
             ? <h1 className="text-5xl font-extralight">Good {getTimeOfDay()}, <span className="text-blue-500 font-bold">{name}</span></h1>
             : <h1 className="text-5xl font-bold">Good {getTimeOfDay()}</h1>}
         <div className="grid grid-cols-4 grid-rows-2 gap-4">
-            <UrgentTaskCounter count={1} date={"November 27, 2024"} className="col-span-3" />
-            <TaskCounter iconUrl="/assets/icons/summary/on-board.svg" name={"Tasks on board"} count={5} />
+            <UrgentTaskCounter count={getUrgentTasks(taskList).length} date={"November 27, 2024"} className="col-span-3" />
+            <TaskCounter iconUrl="/assets/icons/summary/on-board.svg" name={"Tasks on board"} count={taskList.length} />
 
-            <BigTaskCounter count={1} />
-            <TaskCounter iconUrl="/assets/icons/summary/in-progress.svg" name={"Tasks in progress"} count={2} />
-            <TaskCounter iconUrl="/assets/icons/summary/awaiting-feedback.svg" name={"Awaiting feedback"} count={2} />
-            <TaskCounter iconUrl="/assets/icons/summary/tasks-done.svg" name={"Tasks done"} count={5} />
+            <BigTaskCounter count={getTasks('To do', taskList).length} />
+            <TaskCounter iconUrl="/assets/icons/summary/in-progress.svg" name={"Tasks in progress"} count={getTasks('In progress', taskList).length} />
+            <TaskCounter iconUrl="/assets/icons/summary/awaiting-feedback.svg" name={"Awaiting feedback"} count={getTasks('Awaiting feedback', taskList).length} />
+            <TaskCounter iconUrl="/assets/icons/summary/tasks-done.svg" name={"Tasks done"} count={getTasks('Done', taskList).length} />
         </div>
     </div>
 );
 
 const Summary = () => {
+    const { taskList } = useTasks();
+
     return (
         <div className="page-content flex items-center justify-center">
             <MobileSwitch
                 mobileComponent={<MobileSummary />}
-                desktopComponent={<DesktopSummary name="Artir Guri"/>} />
+                desktopComponent={<DesktopSummary name="Artir Guri" taskList={taskList}/>} />
         </div>
     );
 }
