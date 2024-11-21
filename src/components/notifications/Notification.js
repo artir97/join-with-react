a351720525e5b3588758c76dd26cd7b1fe61d91b
Guy Luong <guy.luong.7@gmail.com>
2024-11-21 16:04:58 +0100

    [Feature] Notification system

---
+++ src/components/notifications/Notification.js
@@ -0,0 +1,31 @@
+import { useNotifications } from "../../hooks/useDataContext";
+import { getEnvironmentLink } from "../../tools/navigation";
+
+const getColorClassFromType = (type) => {
+    switch (type) {
+        case "info": return "bg-blue-500";
+        case "error": return "bg-red-500";
+        default: throw new Error(`Notification: Type does not have a color class associated: ${type}`);
+    }
+}
+
+/**
+ * 
+ * @param {Object} param0
+ * @param {string} type - [`"info"`, `"error"`] - Type of notification. Effectively changes the color of the push notification. 
+ * @param {string} message - Content of the notification
+ */
+const Notification = ({ id, type = "info", message = "" }) => {
+    const { closeNotification } = useNotifications();
+
+    return (
+        <div className={`flex items-center space-x-2 px-2 py-1 rounded ${getColorClassFromType(type)} text-white duration-300 ease-in-out`}>
+            <p>{message}</p>
+            <img className="size-4" src={getEnvironmentLink("assets/icons/forms/close-white.svg")}
+                alt="Close icon"
+                onClick={() => closeNotification(id)} />
+        </div>
+    );
+}
+
+export default Notification;
---
+++ src/contexts/NotificationContext.js
@@ -0,0 +1,55 @@
+import { createContext, useState } from "react";
+
+const TIMEOUT = 3000; // 3 seconds
+let nextId = 0;
+
+export const NotificationContext = createContext(null);
+
+export const NotificationProvider = ({ children }) => {
+    const [data, setData] = useState({ notifications: [] });
+
+    const pushNotification = ({ type, message }) => {
+        const notification = { id: nextId++, type, message };
+
+        console.log("Pushed notification", message, type);
+
+        setData(data => {
+            return { ...data, notifications: [...data.notifications, notification] }
+        });
+        setTimeout(() => {
+            setData(data => {
+                return {
+                    ...data,
+                    // This would have worked if there is no way to close the notification early.
+                    // notifications: data.notifications.slice(1)
+                    notifications: data.notifications.filter(n => n.id !== notification.id)
+                }
+            });
+        }, TIMEOUT);
+    }
+
+    const pushNotificationInfo = (message) => {
+        pushNotification({ type: "info", message });
+    }
+
+    const pushNotificationError = (message) => {
+        pushNotification({ type: "error", message });
+    }
+
+    const closeNotification = (id) => {
+        setData(data => {
+            return {
+                ...data,
+                // This would have worked if there is no way to close the notification early.
+                // notifications: data.notifications.slice(1)
+                notifications: data.notifications.filter(n => n.id !== id)
+            }
+        });
+    }
+
+    return (
+        <NotificationContext.Provider value={{ ...data, pushNotificationInfo, pushNotificationError, closeNotification }}>
+            {children}
+        </NotificationContext.Provider>
+    )
+}
