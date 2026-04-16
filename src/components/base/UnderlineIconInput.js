9af85355e7139841940bf23b80b23b5d47183c87
Guy Luong <16323642+firisthetraveller@users.noreply.github.com>
2026-04-16 16:29:03 +0200

    [Fix] Password is now hidden

--- src/components/base/UnderlineIconInput.js
+++ src/components/base/UnderlineIconInput.js
@@ -1,11 +1,12 @@
 import IconInput from "./IconInput"
 
-const UnderlineIconInput = ({ value, large = false, onChange = () => {}, required = false, placeholder = "", icon = null, iconUrl = "" }) => {
+const UnderlineIconInput = ({ value, type, large = false, onChange = () => {}, required = false, placeholder = "", icon = null, iconUrl = "" }) => {
     return (
         <IconInput
             value={value} onChange={onChange}
             required={required}
             placeholder={placeholder}
+            type={type}
             className="font-light"
             containerClassName={`w-full ${large ? "" : "lg:w-3/4"} border-b p-1`}
             onFocusContainerClassName="border-blue-500"
