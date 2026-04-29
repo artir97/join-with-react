e3857e9658d980ede7ab68a2155fcc488481feb9
Guy Luong <16323642+firisthetraveller@users.noreply.github.com>
2026-04-29 11:29:02 +0200

    [Fix] Typo

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -7,11 +7,11 @@ import { backOrDefault, getEnvironmentLink } from "../tools/navigation";
 
 const contents = [
     {
-        header: "Subtitile",
+        header: "Subtitle",
         texts: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio felis, iaculis ut massa eget, ornare lacinia urna. In dignissim justo eu velit sagittis, in scelerisque nulla convallis. Vestibulum eros lorem, sollicitudin eget eros non, varius aliquam mauris. Sed turpis ipsum, condimentum quis nulla at, lobortis facilisis ipsum. Nunc erat justo, hendrerit vel enim vitae, feugiat mattis dui. In auctor dignissim luctus. Mauris ornare ipsum at ultrices eleifend. Praesent tempus congue magna. Quisque libero erat, pharetra a neque et, imperdiet semper justo."]
     },
     {
-        header: "Subtitile",
+        header: "Subtitle",
         texts: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio felis, iaculis ut massa eget, ornare lacinia urna. In dignissim justo eu velit sagittis, in scelerisque nulla convallis. Vestibulum eros lorem, sollicitudin eget eros non, varius aliquam mauris. Sed turpis ipsum, condimentum quis nulla at, lobortis facilisis ipsum. Nunc erat justo, hendrerit vel enim vitae, feugiat mattis dui. In auctor dignissim luctus. Mauris ornare ipsum at ultrices eleifend. Praesent tempus congue magna. Quisque libero erat, pharetra a neque et, imperdiet semper justo."]
     }
 ]
